b45cd6ccae9059d706051488183749f3a5dc7ed7
bradly0cjw <57744542+bradly0cjw@users.noreply.github.com>
2022-06-15 18:35:45 +0800

    Delete rename.sh

--- rename.sh
+++
@@ -1,6 +0,0 @@
-git filter-branch -f --env-filter "
-    GIT_AUTHOR_NAME='bradly0cjw'
-    GIT_AUTHOR_EMAIL='bradly093@gmail.com'
-    GIT_COMMITTER_NAME='bradly0cjw'
-    GIT_COMMITTER_EMAIL='bradly093@gmail.com'
-  " HEAD
